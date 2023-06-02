6837905957dd9ebac3a39b88d877b92675bdcb0f
Syed Mushahid <13629@students.riu.edu.pk>
2023-06-02 22:14:14 +0500

    icons set

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function Hero(){
-
-    return (
-        <>
-          <section class="hero" id="home">
-            <div class="overlay">
-              <div class="content">
-                <h1 class="text-white" style={{fontSize:'46px !important'  }}>
-                  WIR LASSEN SIE NICHT
-                  <br />
-
-                  <strong class="highlight">“LINKS LIEGEN” !</strong>
-                </h1>
-                <button className="hero-btn"style={{ borderRadius: "40px", marginTop: "30px" }}>
-                  Alle Dienste anzeign
-                </button>
-              </div>
-            </div>
-          </section>
-        </>
-    )
+export default function Hero() {
+  return (
+    <>
+      <section class="hero" id="home">
+        <div class="overlay">
+          <div class="content">
+            <h1 class="text-white" style={{ fontSize: "46px !important" }}>
+              WIR LASSEN SIE NICHT
+              <br />
+              <strong class="highlight">“LINKS LIEGEN” !</strong>
+            </h1>
+            <a href="#services">
+              <button
+                className="hero-btn"
+                style={{ borderRadius: "40px", marginTop: "30px" }}
+              >
+                Alle Dienste anzeign
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
